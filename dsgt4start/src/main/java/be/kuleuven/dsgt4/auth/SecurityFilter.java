8783683f1c83e620463274bbc426ac7b98831052
roottoot <koenraad.goddefroy@student.kuleuven.be>
2024-05-23 11:46:16 +0200

    added securityFilter and WebSecurityConfig but giving trouble with imports on this pc

--- dsgt4start/src/main/java/be/kuleuven/dsgt4/auth/SecurityFilter.java
+++ dsgt4start/src/main/java/be/kuleuven/dsgt4/auth/SecurityFilter.java
@@ -8,15 +8,20 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Autowired;
 
 // for level 1 authentication
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.interfaces.DecodedJWT;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import java.security.interfaces.RSAPublicKey;
+import com.fasterxml.jackson.databind.ObjectMapper;
 // till here
 
 
@@ -33,64 +38,108 @@ import java.util.List;
 public class SecurityFilter extends OncePerRequestFilter {
 
     // added to implement level 1 of the authentication
+    private WebClient.Builder webCliebntBuilder;
+    private boolean isManager;
+
+    protected boolean verifyJWT(DecodedJWT decodedJWT) {
+        ObjectMapper objectMapper = new ObjectMApper();
+        var keyData = objectMapper.readTree(getPublicKey());
+        var publicKey = keyData.get(decodedJWT.getKeyId()).asText();
+
+        var rawKey = publicKey.replace("-----BEGIN CERTIFICATE-----", "")
+                .replace("-----END CERTIFICATE-----", "")
+                .replace("\n", "");
+        // Going from raw key strring to RSA256 format
+        byte keyBytes[] = Base64.getDecoder().decode(rawKey);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(keyBytes);
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(byteArrayInputStream);
+
+        Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) x509KeySpec.getPublicKey(), null);
+
+        // Now JWT will be decided if valid or not
+        JWT.require(algorithm)
+                .build()
+                .verify(decodedJWT);
+        System.out.println("verification succesfull");
+        return true;
+    }
+    catch{(Exception e)
+        System.out.println("verification failed: " + e.getMessage());
+    }
+
+    }
+
+
+    public Strring getPublicKey(){
+    var publicKey = webClientBuilder
+            .baseUrl("fillInRightUrl")
+            .build()
+            .get()
+            .retrieve()
+            .bodyToMono(String.class)
+            .block();
+    return publicKey;
+    }
+
+
+
+
     public static String extractIdentityTokenFromRequest(HttpServletRequest request) {
         // Assuming the identity token is in the "Authorization" header
         String authorizationHeader = request.getHeader("Authorization");
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             // Extract the token part after "Bearer "
-            return authorizationHeader.substring(7);
+            return authorizationHeader.substring("Bearer ".lentth());
         }
-        ///////////////This method should be reviewed (but just to get rid of errors now)
-        // If not found in the header, check other places (e.g., query parameters)
-        // Implement additional logic here based on your specific use case
-
         // Return null if no identity token found
         return null;
-    }
-    public static DecodedJWT decodeIdentityToken(String token) {
-        String[] chunks = token.split("\\.");
 
-        if (chunks.length < 2) {
-            // Invalid token format
-            return null;
-        }
-
-        Base64.Decoder decoder = Base64.getUrlDecoder();
-        String header = new String(decoder.decode(chunks[0]));
-        String payload = new String(decoder.decode(chunks[1]));
-
-        // Create a DecodedJWT object
-        return JWT.decode(token);
-    }
-
-    //till here
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // TODO: (level 1) decode Identity Token and assign correct email and role
         // TODO: (level 2) verify Identity Token
 
-        //Implemented for level 1
+
         try {
-            String identityToken = extractIdentityTokenFromRequest(request);
-            DecodedJWT decodedJWT = decodeIdentityToken(identityToken);
+            String token = extractIdentityTokenFromRequest(request);
+            DecodedJWT decodedJWT = JWT.decode(token);
 
             if (decodedJWT != null) {
-                String userEmail = decodedJWT.getSubject();
+                String userEmail = decodedJWT.getClaim("email").asString();
                 String userRole = decodedJWT.getClaim("role").asString();
-                //I think this has to go for the next line //var user = new User("test@example.com", "manager");
+                //this has to go for the next line //var user = new User("test@example.com", "manager");
                 var user = new User(userEmail, userRole);
+
+                FirebaseAuthentication firebaseAuthentication = new FirebaseAuthentication(user);
+
+                if (isManager){
+                    boolean verified = verifyJWT(decodedJWT);
+                    if (verified){
+                        firebaseAuthentication.setAuthentication(true);
+                    }
+                    else{
+                        firebaseAuthentication.setAuthentication(false);
+                    }
+                }
+                else {
+                    firebaseAuthentication.setAuthentication(true);
+                }
+
                 SecurityContext context = SecurityContextHolder.getContext();
                 context.setAuthentication(new FirebaseAuthentication(user));
+                // also done at the end but include in try block
+                filterChain.doFilter(request, response);
             }
         } catch (Exception e) {
-            // Handle exceptions (e.g., invalid tokens)
+            // Handle exceptions (like invalid tokens)
             e.printStackTrace();
         }
 
         filterChain.doFilter(request, response);
     }
-        //
+
 
 
     @Override
@@ -101,6 +150,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     private static class FirebaseAuthentication implements Authentication {
         private final User user;
+        private boolean auth;
 
         FirebaseAuthentication(User user) {
             this.user = user;
@@ -132,17 +182,17 @@ public class SecurityFilter extends OncePerRequestFilter {
 
         @Override
         public boolean isAuthenticated() {
-            return true;
+            return auth;
         }
 
         @Override
         public void setAuthenticated(boolean b) throws IllegalArgumentException {
-
+            auth = b;
         }
 
         @Override
         public String getName() {
-            return null;
+            return this.user.getEmail();
         }
     }
 }
