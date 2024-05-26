54232f164ed4b9b616dace501e97345ae2ae04c7
roottoot <koenraad.goddefroy@student.kuleuven.be>
2024-05-26 20:53:19 +0200

    Dellivery connection fixed

--- dsgt4start/src/main/resources/static/index.js
+++ dsgt4start/src/main/resources/static/index.js
@@ -1,183 +1,3 @@
-/*import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
-import {
-  getAuth,
-  connectAuthEmulator,
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
-
-// Setup authentication and wire up events
-setupAuth();
-if (document.getElementById("btnSignIn")) {
-  wireGuiUpEvents();
-}
-wireUpAuthChange();
-
-// Setup authentication with local or cloud configuration
-function setupAuth() {
-  let firebaseConfig;
-  if (location.hostname === "localhost") {
-    firebaseConfig = {
-      apiKey: "AIzaSyBoLKKR7OFL2ICE15Lc1-8czPtnbej0jWY",
-      projectId: "demo-distributed-systems-kul",
-    };
-  } else {
-    firebaseConfig = {
-      // TODO: for level 2, paste your config here
-    };
-  }
-
-  const firebaseApp = initializeApp(firebaseConfig);
-  const auth = getAuth(firebaseApp);
-  try {
-    auth.signOut();
-  } catch (err) { }
-
-  if (location.hostname === "localhost") {
-    connectAuthEmulator(auth, "http://localhost:8082", { disableWarnings: true });
-  }
-}
-
-function wireGuiUpEvents() {
-  var email = document.getElementById("email");
-  email.textContent = "HAAAAKKK";
-  var password = document.getElementById("password");
-  var signInButton = document.getElementById("btnSignIn");
-  var signUpButton = document.getElementById("btnSignUp");
-  var logoutButton = document.getElementById("btnLogout");
-
-  signInButton.addEventListener("click", function () {
-    signInWithEmailAndPassword(getAuth(), email.value, password.value)
-      .then(function () {
-        console.log("signed in");
-        window.location.href = 'Order_page.html';
-      })
-      .catch(function (error) {
-        console.log("error signInWithEmailAndPassword:", error.message);
-        alert(error.message);
-      });
-  });
-
-  signUpButton.addEventListener("click", function () {
-    createUserWithEmailAndPassword(getAuth(), email.value, password.value)
-      .then(function () {
-        console.log("account created");
-      })
-      .catch(function (error) {
-        console.log("error createUserWithEmailAndPassword:", error.message);
-        alert(error.message);
-      });
-  });
-
-  logoutButton.addEventListener("click", function () {
-    try {
-      var auth = getAuth();
-      auth.signOut();
-    } catch (err) { }
-  });
-}
-
-function wireUpAuthChange() {
-  var auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    console.log("onAuthStateChanged");
-    if (!user) {
-      console.log("user is null");
-      if (document.getElementById("logindiv")) {
-        showUnAuthenticated();
-      }
-      return;
-    }
-    if (!auth) {
-      console.log("auth is null");
-      if (document.getElementById("logindiv")) {
-        showUnAuthenticated();
-      }
-      return;
-    }
-    if (!auth.currentUser) {
-      console.log("currentUser is undefined or null");
-      if (document.getElementById("logindiv")) {
-        showUnAuthenticated();
-      }
-      return;
-    }
-
-    auth.currentUser.getIdTokenResult().then((idTokenResult) => {
-      console.log("Hello " + auth.currentUser.email);
-      if (document.getElementById("logindiv")) {
-        showAuthenticated(auth.currentUser.email);
-      }
-
-      console.log("Token: " + idTokenResult.token);
-      if (document.getElementById("contentdiv")) {
-        fetchData(idTokenResult.token);
-
-      }
-
-    });
-  });
-
-}
-
-function fetchData(token) {
-  getHello(token);
-  whoami(token);
-}
-
-function showAuthenticated(username) {
-  document.getElementById("namediv").innerHTML = "Hello " + username;
-  document.getElementById("logindiv").style.display = "none";
-  document.getElementById("contentdiv").style.display = "block";
-}
-
-function showUnAuthenticated() {
-  document.getElementById("namediv").innerHTML = "";
-  //document.getElementById("email").value = "";
-  document.getElementById("password").value = "";
-  document.getElementById("logindiv").style.display = "block";
-  document.getElementById("contentdiv").style.display = "none";
-}
-
-function addContent(text) {
-  var newElement = document.createElement('div');
-  newElement.className = 'content-style';
-  newElement.innerHTML = text;
-  document.getElementById("contentdiv").appendChild(newElement);
-}
-
-function getHello(token) {
-  fetch('/api/hello', {
-    headers: { Authorization: `Bearer ${token}` }
-  })
-    .then((response) => response.text())
-    .then((data) => {
-      console.log(data);
-      addContent(data);
-      document.getElementById("email").value = data;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-
-function whoami(token) {
-  fetch('/api/whoami', {
-    headers: { Authorization: 'Bearer ' + token }
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.email + data.role);
-      addContent("Whoami at rest service: " + data.email + " - " + data.role);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-*/
-//////////////////////////////////////::
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
 import {
   getAuth,
@@ -191,6 +11,7 @@ import {
 setupAuth();
 setupEventHandlers();
 handleAuthStateChanges();
+let globalData;
 
 function setupAuth() {
   const firebaseConfig = location.hostname === "localhost" ? {
@@ -220,6 +41,7 @@ function setupEventHandlers() {
     signInWithEmailAndPassword(getAuth(), emailInput.value, passwordInput.value)
         .then(() => {
           console.log("Signed in successfully");
+          ////////////////////////////  Add check for availability if not go to error page "service not available"
           displayOrderPage();
         })
         .catch(error => {
@@ -253,11 +75,13 @@ function handleAuthStateChanges() {
     user.getIdTokenResult().then(idTokenResult => {
       showAuthenticated(user.email);
       fetchData(idTokenResult.token);
+
     }).catch(err => {
       console.error("Error getting ID token result:", err);
       showUnauthenticated();
     });
   });
+
 }
 
 function fetchData(token) {
@@ -284,15 +108,17 @@ function addContent(text) {
 }
 
 function fetchHello(token) {
-  fetch('/api/hello', {
+  fetch('/api/askDelivery', {
     headers: { Authorization: 'Bearer ' + token }
   })
       .then(response => response.text())
       .then(data => {
         console.log(data);
+
+        globalData=data;
         addContent(data);
       })
-      .catch(error => console.error("Error fetching /api/hello:", error));
+      .catch(error => console.error("Error fetching /api/askDelivery:", error));
 }
 
 function fetchWhoAmI(token) {
@@ -399,6 +225,7 @@ function displayCheckoutPage() {
   `;
   document.body.appendChild(checkoutContent);
   // I still need to add the add to basket function!!!!
+
   const confirmationButton = document.createElement('button');
   confirmationButton.id = 'btnCheckout';
   confirmationButton.innerText = 'Pay';
@@ -420,17 +247,15 @@ function displayCheckoutPage() {
   });
 }
 
-
-
-function displayConfirmationPage(){
+function displayConfirmationPage() {
   // Clear the existing content
   document.body.innerHTML = '';
+
   // Load new CSS file
   const confirmationStyleSheet = document.createElement('link');
   confirmationStyleSheet.rel = 'stylesheet';
   confirmationStyleSheet.href = '../cssFiles/Confirmation_page.css';
   document.head.appendChild(confirmationStyleSheet);
-
 
   // Create new content
   const confirmationContent = document.createElement('div');
@@ -465,6 +290,21 @@ function displayConfirmationPage(){
     </div>
   `;
   document.body.appendChild(confirmationContent);
+
+  // Populate the fields with data from globalData
+  if (globalData) {
+    const data = JSON.parse(globalData); // Assuming globalData is a JSON string
+
+    document.getElementById('orderNumber').innerText = `Your order number: ${data.orderNumber}`;
+    document.getElementById('beer').innerText = `Beer: ${data.beer}`;
+    document.getElementById('wine').innerText = `Wine: ${data.wine}`;
+    document.getElementById('whiskey').innerText = `Whiskey: ${data.whiskey}`;
+    document.getElementById('totalPrice').innerText = `Delivery price: ${data.totalPrice}`;
+    document.getElementById('expectedDeliveryDate').innerText = `Expected delivery date: ${data.deliveryDate}`;
+    document.getElementById('deliveredBy').innerText = `Delivered by: ${data.deliveryPerson.name}`;
+    document.getElementById('phoneNumber').innerText = `PhoneNumber : ${data.deliveryPerson.phoneNumber}`;
+    document.getElementById('email').innerText = `Email : ${data.deliveryPerson.email}`;
+  }
 
   // Re-setup the logout button event handler
   const logoutButton = document.createElement('button');
