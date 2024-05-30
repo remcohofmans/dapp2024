d86d9a4d70e798e930cf56429d1646003017849b
dapp <samih.ouhmad@student.kuleuven.be>
2024-05-30 14:46:45 +0200

    Addition of wine soap service

---
+++ gs-consuming-web-service/complete/src/main/java/com/example/consumingwebservice/SoapConfiguration.java
@@ -0,0 +1,27 @@
+
+package com.example.consumingwebservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class SoapConfiguration {
+
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setContextPath("com.example.consumingwebservice.wsdl");
+		return marshaller;
+	}
+
+	@Bean
+	public SoapClient soapClient(Jaxb2Marshaller marshaller) {
+		SoapClient client = new SoapClient();
+		client.setDefaultUri("http://dapp.uksouth.cloudapp.azure.com:12000/ws");
+		client.setMarshaller(marshaller);
+		client.setUnmarshaller(marshaller);
+		return client;
+	}
+
+}
