35ad72209563ae993b4f06cc050f7a78f967b144
remcohofmans <remco.hofmans@student.kuleuven.be>
2024-06-15 13:33:46 +0200

    "Refactoring (renaming, shuffling of files)"

--- FinalDAPP/dsgt-soap-wine/dsgt-soap-wine/complete/src/main/java/com/example/consumingwebservice/wsdl/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://winemenu.io/gt/webservice", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.example.consumingwebservice.wsdl;
